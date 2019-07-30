15792a293b3ab14a80c295cc7e6ef0482c9e6b3b
zhaoyifei <yifei.zhao@lishang.com>
2019-07-30 15:39:35 +0800

    add 首页底部四个tab

--- js/page/HomePage.js
+++ js/page/HomePage.js
@@ -1,33 +1,75 @@
 import React, { Component } from 'react';
-import {
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation';
+import PopularPage from './PopularPage'
+import TrendingPage from './TrendingPage'
+import FavoritePage from './FavoritePage'
+import MyPage from './MyPage'
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons'
+import Feather from 'react-native-vector-icons/Feather'
+import Entypo from 'react-native-vector-icons/Entypo'
+
 
 export default class HomePage extends Component {
+  _tabNavigator() {
+    return createAppContainer(createBottomTabNavigator({
+      PopularPage: {
+        screen: PopularPage,
+        navigationOptions: {
+          tabBarLabel: '最热',
+          tabBarIcon: ({ tintColor, focused }) => (
+            <MaterialIcons
+              name={'whatshot'}
+              size={26}
+              style={{ color: tintColor }}
+            />
+          )
+        }
+      },
+      TrendingPage: {
+        screen: TrendingPage,
+        navigationOptions: {
+          tabBarLabel: '趋势',
+          tabBarIcon: ({ tintColor, focused }) => (
+            <Feather
+              name={'trending-up'}
+              size={26}
+              style={{ color: tintColor }}
+            />
+          )
+        }
+      },
+      FavoritePage: {
+        screen: FavoritePage,
+        navigationOptions: {
+          tabBarLabel: '收藏',
+          tabBarIcon: ({ tintColor, focused }) => (
+            <MaterialIcons
+              name={'favorite'}
+              size={26}
+              style={{ color: tintColor }}
+            />
+          )
+        }
+      },
+      MyPage: {
+        screen: MyPage,
+        navigationOptions: {
+          tabBarLabel: '我的',
+          tabBarIcon: ({ tintColor, focused }) => (
+            <Entypo
+              name={'user'}
+              size={26}
+              style={{ color: tintColor }}
+            />
+          )
+        }
+      }
+    }))
+  }
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          HomePage
-        </Text>
-      </View>
-    );
+    const Tab = this._tabNavigator()
+    return <Tab />
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-});
 
